1644dde2d66c51df1ebb32e68330ce52785b81cf
Christian Zufferey <christian@zufferey.com>
2024-05-11 00:47:22 +0200

    refact: totalement refactorisé le code ᕗ

--- NFC_reader_test1/NFC_reader_test1.ino
+++ NFC_reader_test1/NFC_reader_test1.ino
@@ -1,7 +1,7 @@
 // Système pour lire une puce NFC et donner des critères de qualité dans une DB API REST avec un esp32-c3-super-mini
 // ATTENTION, ce code a été écrit pour un esp32-c3 super mini. Pas testé sur les autres boards !
 //
-#define zVERSION "zf240510.2252"
+#define zVERSION "zf240511.0033"
 
 //
 // Utilisation:
@@ -24,44 +24,28 @@
 // https://www.aliexpress.com/item/1005006005040320.html
 // https://randomnerdtutorials.com/esp32-useful-wi-fi-functions-arduino
 //
+/*
+ * 
+ * Pin layout used:
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      ESP32-c3
+ *             Reader/PCB   super mini
+ * Signal      Pin          Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          GPIO8
+ * SPI SS      SDA(SS)      GPIO7
+ * SPI MOSI    MOSI         GPIO6
+ * SPI MISO    MISO         GPIO5
+ * SPI SCK     SCK          GPIO4
+ *
+ */
 
 
 // #define DEBUG true
 // #undef DEBUG
 
 
-/*
- * --------------------------------------------------------------------------------------------------------------------
- * Example sketch/program showing how to read new NUID from a PICC to serial.
- * --------------------------------------------------------------------------------------------------------------------
- * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
- * 
- * Example sketch/program showing how to the read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
- * Reader on the Arduino SPI interface.
- * 
- * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
- * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
- * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
- * will show the type, and the NUID if a new card has been detected. Note: you may see "Timeout in communication" messages
- * when removing the PICC from reading distance too early.
- * 
- * @license Released into the public domain.
- * 
- * Typical pin layout used:
- * -----------------------------------------------------------------------------------------
- *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
- *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
- * Signal      Pin          Pin           Pin       Pin        Pin              Pin
- * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
- * SPI SS      SDA(SS)      10            53        D10        10               10
- * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
- * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
- * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
- *
- * More pin layouts for other boards can be found here: https://github.com/miguelbalboa/rfid#pin-layout
- */
-
+// MFRC522
 #include <SPI.h>
 #include <MFRC522.h>
 
@@ -69,11 +53,29 @@
 #define RST_PIN 8
  
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
-
 MFRC522::MIFARE_Key key; 
 
 // Init array that will store new NUID 
 byte nuidPICC[4];
+
+
+// Helper routine to dump a byte array as hex values to Serial. 
+void printHex(byte *buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+      USBSerial.print(buffer[i] < 0x10 ? " 0" : " ");
+      USBSerial.print(buffer[i], HEX);
+    }
+}
+
+
+// Helper routine to dump a byte array as dec values to Serial.
+void printDec(byte *buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+      USBSerial.print(' ');
+      USBSerial.print(buffer[i], DEC);
+    }
+}
+
 
 void setup() { 
     USBSerial.begin(19200);
@@ -81,85 +83,65 @@ void setup() {
     delay(3000);  //le temps de passer sur la Serial Monitor ;-)
     USBSerial.println("\n\n\n\n**************************************\nCa commence !\n");
 
-  SPI.begin(); // Init SPI bus
-  rfid.PCD_Init(); // Init MFRC522 
+    SPI.begin(); // Init SPI bus
+    rfid.PCD_Init(); // Init MFRC522 
 
-  for (byte i = 0; i < 6; i++) {
-    key.keyByte[i] = 0xFF;
-  }
+    for (byte i = 0; i < 6; i++) {
+      key.keyByte[i] = 0xFF;
+    }
 
-  USBSerial.println(F("This code scan ALL MIFARE NUID ;-)"));
-  USBSerial.print(F("Using the following key:"));
-  printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+    USBSerial.println(F("This code scan ALL MIFARE UID ᕗ"));
+    USBSerial.print(F("Using the following key:"));
+    printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
 }
  
 void loop() {
 
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-  if ( ! rfid.PICC_IsNewCardPresent())
-    return;
+    // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+    if ( ! rfid.PICC_IsNewCardPresent())
+      return;
 
-  // Verify if the NUID has been readed
-  if ( ! rfid.PICC_ReadCardSerial())
-    return;
+    // Verify if the NUID has been readed
+    if ( ! rfid.PICC_ReadCardSerial())
+      return;
 
-  USBSerial.print(F("\n\n*****************\nPICC type: "));
-  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-  USBSerial.println(rfid.PICC_GetTypeName(piccType));
+    USBSerial.print(F("\n\n*****************\nPICC type: "));
+    MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+    USBSerial.println(rfid.PICC_GetTypeName(piccType));
 
-  // Check is the PICC of Classic MIFARE type
-  // if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
-  //   piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-  //   piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-  //   USBSerial.println(F("Your tag is not of type MIFARE Classic."));
-  //   return;
-  // }
+    // Check is the PICC of Classic MIFARE type
+    // if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    //   piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    //   piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    //   USBSerial.println(F("Your tag is not of type MIFARE Classic."));
+    //   return;
+    // }
 
-  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
-    rfid.uid.uidByte[1] != nuidPICC[1] || 
-    rfid.uid.uidByte[2] != nuidPICC[2] || 
-    rfid.uid.uidByte[3] != nuidPICC[3] ) {
-    USBSerial.println(F("A new card has been detected."));
+    if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+      rfid.uid.uidByte[1] != nuidPICC[1] || 
+      rfid.uid.uidByte[2] != nuidPICC[2] || 
+      rfid.uid.uidByte[3] != nuidPICC[3] ) {
+      USBSerial.println(F("A new card has been detected."));
 
-    // Store NUID into nuidPICC array
-    for (byte i = 0; i < 4; i++) {
-      nuidPICC[i] = rfid.uid.uidByte[i];
+      // Store NUID into nuidPICC array
+      for (byte i = 0; i < 4; i++) {
+        nuidPICC[i] = rfid.uid.uidByte[i];
+      }
+    
+      USBSerial.println(F("The NUID tag is:"));
+      USBSerial.print(F("In hex: "));
+      printHex(rfid.uid.uidByte, rfid.uid.size);
+      USBSerial.println();
+      USBSerial.print(F("In dec: "));
+      printDec(rfid.uid.uidByte, rfid.uid.size);
+      USBSerial.println();
     }
-   
-    USBSerial.println(F("The NUID tag is:"));
-    USBSerial.print(F("In hex: "));
-    printHex(rfid.uid.uidByte, rfid.uid.size);
-    USBSerial.println();
-    USBSerial.print(F("In dec: "));
-    printDec(rfid.uid.uidByte, rfid.uid.size);
-    USBSerial.println();
-  }
-  else USBSerial.println(F("Card read previously."));
+    else USBSerial.println(F("Card read previously."));
 
-  // Halt PICC
-  rfid.PICC_HaltA();
+    // Halt PICC
+    rfid.PICC_HaltA();
 
-  // Stop encryption on PCD
-  rfid.PCD_StopCrypto1();
+    // Stop encryption on PCD
+    rfid.PCD_StopCrypto1();
 }
 
-
-/**
- * Helper routine to dump a byte array as hex values to Serial. 
- */
-void printHex(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    USBSerial.print(buffer[i] < 0x10 ? " 0" : " ");
-    USBSerial.print(buffer[i], HEX);
-  }
-}
-
-/**
- * Helper routine to dump a byte array as dec values to Serial.
- */
-void printDec(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    USBSerial.print(' ');
-    USBSerial.print(buffer[i], DEC);
-  }
-}
