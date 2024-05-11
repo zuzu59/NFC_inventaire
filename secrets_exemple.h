9173a078f84186e784aeeeb7c12fe781bca130af
Christian Zufferey <christian@zufferey.com>
2024-05-11 20:03:05 +0200

    new: ajouté un exemple pour les secrets du projet

---
+++ secrets_exemple.h
@@ -0,0 +1,19 @@
+// zf240511.1953
+// gestion des secrets pour ce projet
+// faire un :
+// cp secrets_exemple.h projet/secrets.h
+// et modifier secrets.h en conséquence
+
+// WIFI
+#define WIFI_SSID "xxx"
+#define WIFI_PASSWORD "xxx"
+
+// MQTT
+#define BROKER_ADDR "xxx.xxx.xxx"
+#define BROKER_USERNAME "xxx"
+#define BROKER_PASSWORD "xxx"
+
+// API REST
+#define apiServerName "https://xxx.xxx.xxx"
+#define apiToken "xxx"
+
