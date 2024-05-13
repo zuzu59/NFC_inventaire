f26fee9fc7adfd406350830aa67adf75bece696c
Christian Zufferey <christian@zufferey.com>
2024-05-13 21:36:46 +0200

    refact: pas mal refactorisé la partie OTA, cela fonctionne super bien. Me reste encore à épurer quelques trucs et après on pourra partir en prod avec ce nouvel outil \o/

--- OTA_test1/OTA_test1.ino
+++ OTA_test1/OTA_test1.ino
@@ -1,7 +1,7 @@
 // Petit test pour voir comment fonctionne l'OTA avec un esp32-c3
 // ATTENTION, ce code a été écrit pour un esp32-c3 super mini. Pas testé sur les autres boards !
 //
-#define zVERSION "zf240513.2001"
+#define zVERSION "zf240513.21.3359"
 /*
 Utilisation:
 
@@ -25,7 +25,9 @@ https://github.com/miguelbalboa/rfid   (elle est vieille mais fonctionne encore 
 
 // General
 const int ledPin = 8;    // the number of the LED pin
-const int zDelay = 100;    // délai pour le blink
+const int zPulseDelayOn = 100;    // délai pour le blink
+const int zPulseDelayOff = 200;    // délai pour le blink
+const int zPulseDelayWait = 1000;    // délai pour le blink
 
 //
 // ATTENTION, il faudra changer la pin RST pour le lecteur RFID RC522 car elle est utilisée par la led builting zf240512.1246
@@ -39,47 +41,33 @@ float rrsiLevel = 0;      // variable to store the RRSI level
 
 #include <WiFi.h>
 #include <WiFiClient.h>
-#include <WebServer.h>
-#include <ESPmDNS.h>
-#include <Update.h>
 
-const char* host = "esp32-c3";
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
-
-WebServer server(80);
-
-/*
- * Login page
- */
-
-#include "loginIndex.h"
-
-
-/*
- * Server Index Page
- */
-
-#include "serverIndex.h"
+const char* host = "esp32-c3";
 
 
 
-/*
- * setup function
- */
+//
+// OTA WEB server
+//
+#include "otaWebServer.h"
+
+
+
+
 void setup(void) {
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH); delay(200); digitalWrite(ledPin, LOW); delay(200);
   digitalWrite(ledPin, HIGH); delay(200); digitalWrite(ledPin, LOW); delay(200);
 
-
   USBSerial.begin(19200);
   USBSerial.setDebugOutput(true);       //pour voir les messages de debug des libs sur la console série !
   delay(3000);  //le temps de passer sur la Serial Monitor ;-)
-  USBSerial.println("\n\n\n\n**************************************\nCa commence 20.1747 !\n");
+  USBSerial.println("\n\n\n\n**************************************\nCa commence!");
+  USBSerial.println(zVERSION);
 
-
-
+  
 
   // Connect to WiFi network
   WiFi.begin(ssid, password);
@@ -96,57 +84,17 @@ void setup(void) {
   USBSerial.print("IP address: ");
   USBSerial.println(WiFi.localIP());
 
-  /*use mdns for host name resolution*/
-  if (!MDNS.begin(host)) { //http://esp32.local
-    USBSerial.println("Error setting up MDNS responder!");
-    while (1) {
-      delay(1000);
-    }
-  }
-  USBSerial.println("mDNS responder started");
-  /*return index page which is stored in serverIndex */
-  server.on("/", HTTP_GET, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", loginIndex);
-  });
-  server.on("/serverIndex", HTTP_GET, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", serverIndex);
-  });
-  /*handling uploading firmware file */
-  server.on("/update", HTTP_POST, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
-    ESP.restart();
-  }, []() {
-    HTTPUpload& upload = server.upload();
-    if (upload.status == UPLOAD_FILE_START) {
-      USBSerial.printf("Update: %s\n", upload.filename.c_str());
-      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
-        Update.printError(Serial);
-      }
-    } else if (upload.status == UPLOAD_FILE_WRITE) {
-      /* flashing firmware to ESP*/
-      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
-        Update.printError(Serial);
-      }
-    } else if (upload.status == UPLOAD_FILE_END) {
-      if (Update.end(true)) { //true to set the size to the current progress
-        USBSerial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-      } else {
-        Update.printError(Serial);
-      }
-    }
-  });
-  server.begin();
+  // start OTA server
+  otaWebServer();
 }
 
+
 void loop(void) {
+  // OTA loop
   server.handleClient();
 
-  digitalWrite(ledPin, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(zDelay);                      // wait for a second
-  digitalWrite(ledPin, LOW);   // turn the LED off by making the voltage LOW
-  delay(zDelay);                      // wait for a second
-
+  digitalWrite(ledPin, LOW); delay(zPulseDelayOn); digitalWrite(ledPin, HIGH); delay(zPulseDelayOff);
+  digitalWrite(ledPin, LOW); delay(zPulseDelayOn); digitalWrite(ledPin, HIGH); delay(zPulseDelayOff);
+  delay(zPulseDelayWait);
+  
 }
